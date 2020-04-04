81e058fa1a03c40a8ddace9a7cbd9af5b0b8ccc2
oziged <oziged@gmail.com>
2020-04-05 01:44:00 +0300

    base setup && cursor component

---
+++ components/AppCursor.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="app-cursor">
+    <div class="cursor-point">
+    </div>
+    <div class="cursor-circle-container">
+      <div :class="{'cursor-circle': true, 'cursor-circle-small': cursor_circle_small}"></div>
+      <div :class="{'pulse-circle': true, pulsing}" ref="pulse-circle"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap';
+
+export default {
+  data() {
+    return {
+      listeners: [],
+      interval: null,
+      clientX: 0,
+      clientY: 0,
+      delayedX: 0,
+      delayedY: 0,
+      pulsing: false,
+      cursor_circle_small: false
+    }
+  },
+
+
+  mounted() {
+    this.enableListeners()
+  },
+
+
+  methods: {
+    enableListeners() {
+      this.listeners.push(document.addEventListener('mousemove', e => {
+        if (!this.interval) this.enableInterval()
+        this.clientX = e.clientX
+        this.clientY = e.clientY
+      }))
+
+      this.listeners.push(document.addEventListener('mousedown', () => this.cursor_circle_small = true))
+      this.listeners.push(document.addEventListener('mouseup', () => this.cursor_circle_small = false))
+      this.listeners.push(document.addEventListener('click', () => this.createSinglePulse()))
+    },
+
+    enableInterval() {
+      this.interval = setInterval(() => {
+        this.delayedX += (this.clientX-this.delayedX)/10
+        this.delayedY += (this.clientY-this.delayedY)/10
+        gsap.set('.cursor-point', {x: this.clientX-2.5, y: this.clientY-2.5})
+        gsap.set('.cursor-circle-container', {x: this.delayedX-15, y: this.delayedY-15})
+        if (Math.abs(this.clientX-this.delayedX) < .1 && Math.abs(this.clientY-this.delayedY < .001)) this.disableInterval()
+      }, 10)
+    },
+
+    disableInterval() {
+      clearInterval(this.interval)
+      this.interval = null
+    },
+
+    enablePulse() {
+      this.pulsing = true
+    },
+
+    disablePulse() {
+      this.pulsing = false
+    },
+
+    createSinglePulse() {
+      gsap.set(this.$refs['pulse-circle'], {animation: 'freeze'})
+      gsap.fromTo(this.$refs['pulse-circle'], {scale: 0, opacity: 1}, {scale: 2.5, opacity: 0, duration: 1.5, ease: 'power3.out', clearProps: 'all'})
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .app-cursor {
+    pointer-events: none;
+    position: fixed;
+    left: 0;
+    top: 0;
+    mix-blend-mode: difference;
+    .cursor-point {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 5px;
+      height: 5px;
+      border-radius: 50%;
+      background-color: white;
+      will-change: transform;
+    }
+    .cursor-circle-container {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 30px;
+      height: 30px;
+      will-change: transform;
+      .cursor-circle {
+        width: 100%;
+        height: 100%;
+        border: 1.5px solid rgba(255, 255, 255, 0.5);
+        border-radius: 50%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        transition: .3s ease-out;
+        transition-delay: .05s;
+      }
+      .cursor-circle-small {
+        transform: scale(.7);
+      }
+      .pulse-circle {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        transform: scale(0);
+        background-color: rgba(255, 255, 255, 0.363);
+      }
+      .pulsing {
+        animation: pulse-animation 1.5s infinite;
+      }
+    }
+  }
+
+  // animations
+
+  @keyframes pulse-animation {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(2.5);
+      opacity: 0;
+    }
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <nuxt/>
+    <app-cursor ref="cursor"/>
+  </div>
+</template>
+
+<script>
+import AppCursor from '@/components/AppCursor'
+
+export default {
+  components: {AppCursor},
+
+  provide () {
+    return {
+      updateCursor: this.updateCursor
+    }
+  },
+
+
+  methods: {
+    updateCursor(type, payload) {
+      this.$refs.cursor[type]()
+    }
+  },
+}
+</script>
+
+<style>
+html {
+  cursor: none;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
+
